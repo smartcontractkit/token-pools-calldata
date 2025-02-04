02ed7c02dce3ef6f809c1ef50f5464d6f9fa348e
0xbleibig <ben.leibig@smartcontract.com>
2025-02-04 22:16:57 +0100

    remove temp files

--- ops/SHIB/04-02-2024-connect-avax/generate.sh
+++ ops/SHIB/04-02-2024-connect-avax/generate.sh
@@ -6,7 +6,7 @@ ARB_TOKEN_POOL=0x6f93AD7963BBdD8C655A0C819B9b79347EE04b70
 AVAX_TOKEN_POOL=0x6f6F5645B86b1fD3c4C015822a0E672132D4e2F8
 BASE_TOKEN_POOL=0xC026ae03C857093979872C665b13dBBA83B55987
 BSC_TOKEN_POOL=0x7f1f90E6b6BAD9fc14ca71224B072541B739beb3
-
+MAINNET_TOKEN_POOL=0x3eC718a22B268d7d9Ce27D2dcAB791174D515920
 # Generate chain update calldata for non-avax chains
 pnpm start generate-chain-update -i ops/SHIB/04-02-2024-connect-avax/inputs/otherchains.json -o ops/SHIB/04-02-2024-connect-avax/outputs/otherchains.calldata.txt
 sed s/--SAFE--/$SAFE/g calldata.template.tx_builder.json > tmp1.json
@@ -24,3 +24,5 @@ pnpm start generate-chain-update -i ops/SHIB/04-02-2024-connect-avax/inputs/avax
 sed s/--SAFE--/$SAFE/g calldata.template.tx_builder.json > tmp1.json
 sed s/--CALLDATA--/$(cat outputs/avax.calldata.txt | tr -d '\n')/g tmp1.json > tmp2.json
 sed s/--TOKENPOOL--/$AVAX_TOKEN_POOL/g tmp2.json > outputs/avax.tx_builder.json
+
+rm tmp1.json tmp2.json
