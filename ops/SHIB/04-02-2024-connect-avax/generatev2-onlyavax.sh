c1e5e602f42e23654c8b34a56bcdd0fff247b3a9
0xbleibig <ben.leibig@smartcontract.com>
2025-02-05 15:07:13 +0100

    Fix output path

--- ops/SHIB/04-02-2024-connect-avax/generatev2-onlyavax.sh
+++ ops/SHIB/04-02-2024-connect-avax/generatev2-onlyavax.sh
@@ -14,11 +14,11 @@ MAINNET_TOKEN_POOL=0x3eC718a22B268d7d9Ce27D2dcAB791174D515920
 
 
 ## Generate chain update calldata for non-avax chains
-pnpm start generate-chain-update  --format safe-json --chain-id $ARB --safe $SAFE --token-pool $ARB_TOKEN_POOL --input ops/SHIB/04-02-2024-connect-avax/inputs/otherchains.json --output arb.tx_builder.json
-pnpm start generate-chain-update  --format safe-json --chain-id $AVALANCHE --safe $SAFE --token-pool $AVAX_TOKEN_POOL --input ops/SHIB/04-02-2024-connect-avax/inputs/otherchains.json --output avax.tx_builder.json
-pnpm start generate-chain-update  --format safe-json --chain-id $BASE --safe $SAFE --token-pool $BASE_TOKEN_POOL --input ops/SHIB/04-02-2024-connect-avax/inputs/otherchains.json --output base.tx_builder.json
-pnpm start generate-chain-update  --format safe-json --chain-id $BNB --safe $SAFE --token-pool $BSC_TOKEN_POOL --input ops/SHIB/04-02-2024-connect-avax/inputs/otherchains.json --output bsc.tx_builder.json
+pnpm start generate-chain-update  --format safe-json --chain-id $ARB --safe $SAFE --token-pool $ARB_TOKEN_POOL --input ops/SHIB/04-02-2024-connect-avax/inputs/otherchains.json --output ops/SHIB/04-02-2024-connect-avax/outputs/arb.tx_builder.json
+pnpm start generate-chain-update  --format safe-json --chain-id $AVALANCHE --safe $SAFE --token-pool $AVAX_TOKEN_POOL --input ops/SHIB/04-02-2024-connect-avax/inputs/otherchains.json --output ops/SHIB/04-02-2024-connect-avax/outputs/avax.tx_builder.json
+pnpm start generate-chain-update  --format safe-json --chain-id $BASE --safe $SAFE --token-pool $BASE_TOKEN_POOL --input ops/SHIB/04-02-2024-connect-avax/inputs/otherchains.json --output ops/SHIB/04-02-2024-connect-avax/outputs/base.tx_builder.json
+pnpm start generate-chain-update  --format safe-json --chain-id $BNB --safe $SAFE --token-pool $BSC_TOKEN_POOL --input ops/SHIB/04-02-2024-connect-avax/inputs/otherchains.json --output ops/SHIB/04-02-2024-connect-avax/outputs/bsc.tx_builder.json
 
 ## Generate chain update calldata for avax chain
 
-pnpm start generate-chain-update  --format safe-json --chain-id $AVALANCHE --safe $SAFE --token-pool $AVAX_TOKEN_POOL --input ops/SHIB/04-02-2024-connect-avax/inputs/avax.json --output avax.tx_builder.json
+pnpm start generate-chain-update  --format safe-json --chain-id $AVALANCHE --safe $SAFE --token-pool $AVAX_TOKEN_POOL --input ops/SHIB/04-02-2024-connect-avax/inputs/avax.json --output ops/SHIB/04-02-2024-connect-avax/outputs/avax.tx_builder.json
