e5115dc56d00e844e94df4527b1d8f2a6338c306
0xbleibig <ben.leibig@smartcontract.com>
2025-03-17 23:49:28 +0100

    Configs to apply chain updates for vertex
    Need to check addresses are the right pools/etc

---
+++ ops/Vertex/generate_configs.bash
@@ -0,0 +1,7 @@
+## Chain updates
+## Arb
+pnpm start generate-chain-update  -i ops/Vertex/apply-avax-mantle-sonic.json -p 0x387e40ed22ee3396288c874411b00c48f6978653     -c 42161   -o  ops/vertex/output/arb-chain-update.txt
+## Base
+pnpm start generate-chain-update  -i ops/Vertex/apply-avax-mantle-sonic.json  -p 0xcaf4e8ed1cbdd5fb9b1359e98d9185dafe01b943     -c 8453   -o ops/vertex/output/base-chain-update.txt
+## Blast
+pnpm start generate-chain-update  -i ops/Vertex/apply-avax-mantle-sonic.json  -p 0xdf7f1eb75e0ae7e4867f06df3344abcd852c6d23    -c 81457   -o  ops/vertex/output/blast-chain-update.txt
