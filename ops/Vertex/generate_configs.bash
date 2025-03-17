786e5ae682a7ca050ceb19089e2ab8aad4893c4d
0xbleibig <ben.leibig@smartcontract.com>
2025-03-17 23:57:00 +0100

    Change pool addresses to v1.4

--- ops/Vertex/generate_configs.bash
+++ ops/Vertex/generate_configs.bash
@@ -1,7 +1,7 @@
 ## Chain updates
 ## Arb
-pnpm start generate-chain-update  -i ops/Vertex/apply-avax-mantle-sonic.json -p 0x387e40ed22ee3396288c874411b00c48f6978653     -c 42161   -o  ops/vertex/output/arb-chain-update.txt
+pnpm start generate-chain-update  -i ops/Vertex/apply-avax-mantle-sonic.json -p 0xA51e97926D25091d3B9fe888113946d5aae7B793     -c 42161   -o  ops/vertex/output/arb-chain-update.txt ## ARB v1.4 pool
 ## Base
-pnpm start generate-chain-update  -i ops/Vertex/apply-avax-mantle-sonic.json  -p 0xcaf4e8ed1cbdd5fb9b1359e98d9185dafe01b943     -c 8453   -o ops/vertex/output/base-chain-update.txt
+pnpm start generate-chain-update  -i ops/Vertex/apply-avax-mantle-sonic.json  -p 0xd0c34cb5438b624e1af94544e9779f58b8a8bd4e     -c 8453   -o ops/vertex/output/base-chain-update.txt
 ## Blast
-pnpm start generate-chain-update  -i ops/Vertex/apply-avax-mantle-sonic.json  -p 0xdf7f1eb75e0ae7e4867f06df3344abcd852c6d23    -c 81457   -o  ops/vertex/output/blast-chain-update.txt
+pnpm start generate-chain-update  -i ops/Vertex/apply-avax-mantle-sonic.json  -p 0xa27836c0bfe3d05772440db19d00b67c22288d40    -c 81457   -o  ops/vertex/output/blast-chain-update.txt
